5ebd6bf27a397a1c5b2d62142758a4163430988e
Mateusz Iwan <143963948+shieq11@users.noreply.github.com>
2023-11-25 09:01:27 +0100

    coffee_machine

---
+++ testowy.py
@@ -0,0 +1,15 @@
+name = input("What is your name ")
+print(len(name))
+
+def dodawanie(n1, n2):
+    total = n1 + n2
+    return total
+
+print(dodawanie(2, 4))
+
+print(dodawanie(2, 4))
+
+print(dodawanie(2, 4))
+print(dodawanie(3
+                , 4))
+print(dodawanie(1, 4))
