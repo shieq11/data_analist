7e54c8d66eafdb4219949946a49f7117875a6249
Mateusz Iwan <143963948+shieq11@users.noreply.github.com>
2023-11-27 18:14:49 +0100

    study

---
+++ day_18/main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle,Screen
+
+
+tim_the_turtle = Turtle()
+tim_the_turtle.shape("turtle")
+tim_the_turtle.color("red")
+tim_the_turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
